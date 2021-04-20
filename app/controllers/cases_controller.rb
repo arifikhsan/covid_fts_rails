2b395c3a9590ad276198c3c8396ec550ca8b93be
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-04-20 14:58:48 +0700

    remove comment

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -4,9 +4,6 @@ class CasesController < ApplicationController
   def index
     a_month_ago = (Time.now - 1.month).utc.iso8601
     @cases = Case.where(:date_time.gte => a_month_ago).to_a
-
-    # binding.pry
-
   end
 
   private
