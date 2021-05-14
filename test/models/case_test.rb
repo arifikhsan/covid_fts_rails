da9c59687801cbf7be5207d7614fbd648fcef66b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-14 15:10:42 +0700

    case create test

--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class CaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can create case' do
+    new_case = Case.new(
+      key: 1,
+      positive: 1,
+      active: 1,
+      recover: 1,
+      death: 1,
+      positive_cumulative: 1,
+      active_cumulative: 1,
+      recover_cumulative: 1,
+      death_cumulative: 1,
+      last_update: 1,
+      date_time: '2021-03-03T00:00:00.00Z'
+    )
+    assert new_case.save
+  end
 end
