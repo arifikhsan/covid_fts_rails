c42fe5584db91fcb5ba8a19fc1378e23b21e4ebc
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-15 21:55:42 +0700

    assert_select text

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get root_path
+
     assert_response :success
+    assert_select 'h2', 'Backend'
   end
 end
