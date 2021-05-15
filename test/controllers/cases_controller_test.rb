c8fd7ad62a520c064e4d613e3059f5602c974b9d
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-15 19:56:22 +0700

    should return success when database is empty

--- test/controllers/cases_controller_test.rb
+++ test/controllers/cases_controller_test.rb
@@ -1,11 +1,13 @@
 require 'test_helper'
 
 class CasesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-
-    binding.pry
-
+  test 'should return success when database is empty' do
+    Case.delete_all
     get cases_path
+    cases = JSON.parse(response.body)['cases']
+
     assert_response :success
+    assert_equal 30, cases.length
+    assert_not_nil cases.sample['active_cumulative']
   end
 end
